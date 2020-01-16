f6738eeac8e8205ddf7d94e1075f8f46304e2ff0
Daryl Legion <whoamisnowden@gmail.com>
2020-01-16 09:32:22 +0800

    admin graph analytics title

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -72,7 +72,7 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Analytics</h3>
+                        <h3 class="card-title">Analytics - Approved Loans</h3>
                     </div>
                     <div class="card-body">
                         <div class="canvas">
